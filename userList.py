63be36508c2bb1eae4c710d17320785c9cb46df6
tatsuyaking <tatsuya_king@brown.edu>
2025-04-27 12:35:26 -0400

    fixing userIngredients script

--- userList.py
+++ userList.py
@@ -1,24 +1,24 @@
 import json
 
-user_info = {}
+userIngredients = {}
 
 # Get user input for name
 name = input("What is your name? ")
-user_info['name'] = name
+userIngredients['name'] = name
 
 # Get user input for age
 age = input("What is your age? ")
-user_info['age'] = age
+userIngredients['age'] = age
 
 # Get user input for favorite color
 favorite_color = input("What is your favorite color? ")
-user_info['favorite_color'] = favorite_color
+userIngredients['favorite_color'] = favorite_color
 
 # Print the dictionary
-print(user_info)
+print(userIngredients)
 
 # Save the dictionary to a JSON file
-with open('user_info.json', 'w') as file:
-    json.dump(user_info, file)
+with open('userIngredients.json', 'w') as file:
+    json.dump(userIngredients, file)
 
-print("User info saved to user_info.json")
+print("User info saved to userIngredients.json")
