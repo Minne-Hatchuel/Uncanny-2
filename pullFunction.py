174d9749356259de1bccf21f71926ffd7bfe5b12
Minne-Hatchuel <minne_hatchuel@brown.edu>
2025-04-27 12:15:14 -0400

    recipe and ingredients inventory

---
+++ pullFunction.py
@@ -0,0 +1,5 @@
+acquiredIngredients = {'egg': 1, 
+    'tomato': 1,
+    'feta cheese', 'salt', 'pepper', 'bread', 'milk', 'sugar', 'pasta', 'pesto', 'arugula', 'lemon', 'olive oil', 'lemon', 'cucumber', 'onion', 'red wine vinegar', 'parmesan cheese', 'butter', 'bacon', 'bell pepper', 'cumin', 'coriander', 'paprika', 'flour', 'chocolate', 'baking powder'
+
+}
