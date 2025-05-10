80c26419f6b8649ead9f4d355577c713f486ab2e
tatsuyaking <tatsuya_king@brown.edu>
2025-05-10 19:01:39 -0400

    added measurement units to ingredients, so that it is more clear what needs to be filled in

---
+++ detailedInventory.py
@@ -0,0 +1,17 @@
+
+#ingredient name and measurement unit NEED TO FILL IN
+
+detailedIngredients = {'egg':'pieces', 'tomato':'pieces', 'feta cheese':'grams', 'salt': '???', 'pepper': '???', 'bread': 'pieces', 'milk': 'millileters', 'sugar':'tablespoons', 'pasta':'bags?', 'pesto':'???', 'arugula': '???', 'lemon': '???', 'olive oil': '???', 'cucumber': '???', 'onion': '???', 'red wine vinegar': '???', 'parmesan cheese': '???', 'butter': '???', 'bacon': '???', 'bell pepper': '???', 'cumin': '???', 'coriander': '???', 'paprika': '???', 'flour': '???', 'chocolate': '???', 'baking powder': '???'}
+
+detailedRecipeIngredients = {
+    'Greek omelette': {'egg': 1, 'tomato': 1, 'feta cheese': 1, 'pepper': 1, 'salt': 1},
+    'French toast': {'egg': 1, 'bread': 1, 'milk': 1, 'sugar': 1},
+    'Italian pasta salad': {'pasta': 1, 'tomato': 1, 'pesto': 1, 'arugula': 1, 'olive oil': 1, 'lemon': 1, 'pepper': 1, 'salt': 1},
+    'Greek salad': {'cucumber': 1, 'tomato': 1, 'onion': 1, 'feta cheese': 1, 'olive oil': 1, 'red wine vinegar': 1, 'salt': 1, 'pepper': 1},
+    'Pasta carbonara': {'pasta': 1, 'egg': 1, 'parmesan cheese': 1, 'butter': 1, 'bacon': 1, 'salt': 1, 'pepper': 1},
+    'Shakshuka': {'egg': 1, 'tomato': 1, 'bell pepper': 1, 'onion': 1, 'olive oil': 1, 'salt': 1, 'pepper': 1, 'coriander': 1, 'cumin': 1, 'paprika': 1},
+    'Chocolate cake': {'egg': 1, 'sugar': 1, 'flour': 1, 'butter': 1, 'chocolate': 1, 'baking powder': 1},
+    'Lemon cake': {'egg': 1, 'sugar': 1, 'flour': 1, 'butter': 1, 'lemon':1, "baking powder":1}
+}
+
+RecipeNames = ['Greek omelette', 'French toast', 'Italian pasta salad', 'Greek salad', 'Pasta carbonara', 'Shakshuka', 'Chocolate cake', 'Lemon cake']
